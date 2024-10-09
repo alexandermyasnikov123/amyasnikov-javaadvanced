04f5d936b535cc771b85c2cc9bdfb152bd3ee6a4
Alexander Myasnikov <a.myasnikov@stv.dunice.net>
2024-10-09 17:43:11 +0300

    Implement numberOfIdenticalPairs

--- src/main/java/net/dunice/advancedjavaprojectacademy/tasks/block2/Block2.java
+++ src/main/java/net/dunice/advancedjavaprojectacademy/tasks/block2/Block2.java
@@ -178,6 +178,16 @@ public class Block2 implements Block2Interface {
 
     @Override
     public int numberOfIdenticalPairs(int[] nums) {
-        return 0;
+        var pairs = 0;
+
+        for (int i = 0; i < nums.length; ++i) {
+            for (int j = i + 1; j < nums.length; ++j) {
+                if (nums[i] == nums[j]) {
+                    ++pairs;
+                }
+            }
+        }
+
+        return pairs;
     }
 }
