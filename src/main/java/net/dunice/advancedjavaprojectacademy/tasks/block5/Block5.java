5f98786bd44a4dd1db7115c742e397da8738517e
Alexander Myasnikov <a.myasnikov@stv.dunice.net>
2024-10-14 17:29:07 +0300

    Complete 5.5 task

--- src/main/java/net/dunice/advancedjavaprojectacademy/tasks/block5/Block5.java
+++ src/main/java/net/dunice/advancedjavaprojectacademy/tasks/block5/Block5.java
@@ -2,6 +2,11 @@ package net.dunice.advancedjavaprojectacademy.tasks.block5;
 
 import lombok.val;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 import java.util.stream.IntStream;
 
@@ -14,25 +19,21 @@ public class Block5 {
         try {
             Thread.sleep(info.pause());
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            Thread.currentThread().interrupt();
         }
     });
 
-    public static void main(String[] args) {
-        val block5 = new Block5();
-        block5.startSeveralThreadsViaRunnable();
-    }
-
-    private void startThreadViaConstructor() {
-        new Thread(() -> {
+    public void startThreadViaConstructor() {
+        val thread = new Thread(() -> {
             val startInclusive = 0;
             val endExclusive = 11;
             val pause = 500L;
             threadInfoConsumer.accept(new ThreadInfo<>(startInclusive, endExclusive, pause, System.out::println));
-        }).start();
+        });
+        ThreadUtils.startAndJoinAll(thread);
     }
 
-    private void startSeveralThreadsViaRunnable() {
+    public void startSeveralThreadsViaRunnable() {
         Runnable firstPrinter = () -> {
             val startInclusive = 1;
             val endExclusive = 6;
@@ -49,7 +50,70 @@ public class Block5 {
             );
         };
 
-        new Thread(firstPrinter).start();
-        new Thread(secondPrinter).start();
+        ThreadUtils.startAndJoinAll(new Thread(firstPrinter), new Thread(secondPrinter));
+    }
+
+    public void startSeveralThreadsAndIncrementCounter(int repeats) {
+        val startValue = new AtomicInteger();
+        Runnable incrementer = () -> {
+            for (int i = 0; i < repeats; ++i) {
+                val value = startValue.incrementAndGet();
+                System.out.printf("Thread %1s: value is %2d%n", Thread.currentThread(), value);
+            }
+        };
+        val thread1 = new Thread(incrementer);
+        val thread2 = new Thread(incrementer);
+        ThreadUtils.startAndJoinAll(thread1, thread2);
+        System.out.println("The result is " + startValue.get());
+    }
+
+    public void startParallelTasks() {
+        val threads = 10;
+        val executors = Executors.newFixedThreadPool(threads);
+
+        val start = 0;
+        val end = 2;
+        val timeout = 500L;
+
+        for (int i = 0; i < threads; ++i) {
+            val taskNumber = i;
+            executors.submit(() -> threadInfoConsumer.accept(new ThreadInfo<>(
+                    start, end, timeout,
+                    value -> System.out.printf("Task %1d%n", taskNumber))
+            ));
+        }
+        executors.shutdown();
+    }
+
+    private Long evalFactorial() {
+        val requiredNumber = 5L;
+        var result = 1L;
+
+        for (int factor = 2; factor <= requiredNumber; factor++) {
+            result *= factor;
+        }
+
+        return result;
+    }
+
+    public void startTask5UsingFuture() throws ExecutionException, InterruptedException {
+        Callable<Long> factorialFunc = this::evalFactorial;
+
+        val executor = Executors.newSingleThreadExecutor();
+        val future = executor.submit(factorialFunc);
+
+        System.out.printf("Future - Factorial of 5 is %1d%n", future.get());
+
+        executor.shutdown();
+    }
+
+    public void startTask5UsingFutureTask() {
+        val factorialFutureTask = new FutureTask<>(() -> {
+            val factorial = evalFactorial();
+            System.out.printf("Future task - Factorial of 5 is %1d%n", factorial);
+            return factorial;
+        });
+
+        ThreadUtils.startAndJoinAll(new Thread(factorialFutureTask));
     }
 }
---
+++ src/main/java/net/dunice/advancedjavaprojectacademy/tasks/block5/Block5Launcher.java
@@ -0,0 +1,20 @@
+package net.dunice.advancedjavaprojectacademy.tasks.block5;
+
+import lombok.val;
+
+import java.util.concurrent.ExecutionException;
+
+public class Block5Launcher {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        val block5 = new Block5();
+        val repeats = 12;
+
+        block5.startTask5UsingFutureTask();
+        block5.startTask5UsingFuture();
+
+        block5.startParallelTasks();
+        block5.startSeveralThreadsAndIncrementCounter(repeats);
+        block5.startThreadViaConstructor();
+        block5.startSeveralThreadsViaRunnable();
+    }
+}
